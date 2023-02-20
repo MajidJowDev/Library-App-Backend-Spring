4968a11eff39aac3020bdc2bbbb9487e27bcd262
MajidJowDev <majid.jow@gmail.com>
2023-02-21 01:25:50 +0330

    19- 3. Spring Boot - Book Service Enhancement Added Book checkout method to validate and save a checkout

---
+++ src/main/java/mjz/springlibrary/librarybespring/service/BookService.java
@@ -0,0 +1,53 @@
+package mjz.springlibrary.librarybespring.service;
+
+import mjz.springlibrary.librarybespring.dao.BookRepository;
+import mjz.springlibrary.librarybespring.dao.CheckoutRepository;
+import mjz.springlibrary.librarybespring.entity.Book;
+import mjz.springlibrary.librarybespring.entity.Checkout;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+
+@Service
+@Transactional
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final CheckoutRepository checkoutRepository;
+
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+        this.bookRepository = bookRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Book checkoutBook (String userEmail, Long bookId) throws Exception {
+
+        Optional<Book> book =bookRepository.findById(bookId);
+
+        // we want a user can only check out a book one time
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0) {
+            throw new Exception("Book doesn't exist or already checked out by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+
+        bookRepository.save(book.get());
+
+        Checkout checkout = new Checkout(
+                userEmail,
+                LocalDate.now().toString(),
+                LocalDate.now().plusDays(7).toString(), // days to return the book back
+                book.get().getId()
+                );
+
+        checkoutRepository.save(checkout);
+
+        return  book.get();
+    }
+
+}
