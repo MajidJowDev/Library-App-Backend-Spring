4f2a81475ef41e9857eee99a278023466a3d14b2
MajidJowDev <majid.jow@gmail.com>
2023-02-21 17:17:40 +0330

    20- 2. Spring Boot - Security Configurations added SecurityFilterChain to configuration

---
+++ src/main/java/mjz/springlibrary/librarybespring/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package mjz.springlibrary.librarybespring.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+@EnableWebSecurity // we had to add this because of the bug in intellij version 2021.3, can be removed in newer version
+public class SecurityConfiguration {
+
+    //returns a security filter chain
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //disable cross site request forgery
+        http.csrf().disable();
+
+        //protect endpoints at /api/<type>/secure
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .antMatchers("/api/books/secure/**") // wild card for anything after secure/
+                        .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
+
+        //Add CORS filters
+        http.cors();
+
+        //Add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+
+        //Force a non-empty response body for 401's to make the response friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }
+}
