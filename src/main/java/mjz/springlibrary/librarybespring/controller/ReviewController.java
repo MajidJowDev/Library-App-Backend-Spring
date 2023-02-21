9c7cb0a11731e15695dff23b8c02bacadc386369
MajidJowDev <majid.jow@gmail.com>
2023-02-22 02:11:16 +0330

    22- 4. Spring Boot - Review Controller

---
+++ src/main/java/mjz/springlibrary/librarybespring/controller/ReviewController.java
@@ -0,0 +1,30 @@
+package mjz.springlibrary.librarybespring.controller;
+
+import mjz.springlibrary.librarybespring.requestmodels.ReviewRequest;
+import mjz.springlibrary.librarybespring.service.ReviewService;
+import mjz.springlibrary.librarybespring.utils.ExtractJWT;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody ReviewRequest reviewRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if(userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
