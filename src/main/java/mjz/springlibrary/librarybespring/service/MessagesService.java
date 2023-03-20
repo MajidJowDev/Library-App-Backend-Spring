c0cee1dde193d1bdcafcc86edcf5b23b41005fb2
MajidJowDev <majid.jow@gmail.com>
2023-03-20 16:18:07 +0330

    27- 12. Spring Boot - Enhance Messages Service Added update method for messages (for admin users)

--- src/main/java/mjz/springlibrary/librarybespring/service/MessagesService.java
+++ src/main/java/mjz/springlibrary/librarybespring/service/MessagesService.java
@@ -2,8 +2,11 @@ package mjz.springlibrary.librarybespring.service;
 
 import mjz.springlibrary.librarybespring.dao.MessageRepository;
 import mjz.springlibrary.librarybespring.entity.Message;
+import mjz.springlibrary.librarybespring.requestmodels.AdminQuestionRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -19,6 +22,19 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest, String userEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminQuestionRequest.getId());
+
+        if(!message.isPresent()) {
+            throw new Exception("Message not found");
+        }
+
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 
 }
