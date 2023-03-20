90e9b993020c8a9ed8b85f0df19b8d61c901096f
MajidJowDev <majid.jow@gmail.com>
2023-03-20 16:27:54 +0330

    27- 13. Spring Boot - Enhance Messages Controller Endpoint Added update message endpoint to MessagesController (for Admin users only)

--- src/main/java/mjz/springlibrary/librarybespring/controller/MessagesController.java
+++ src/main/java/mjz/springlibrary/librarybespring/controller/MessagesController.java
@@ -1,6 +1,7 @@
 package mjz.springlibrary.librarybespring.controller;
 
 import mjz.springlibrary.librarybespring.entity.Message;
+import mjz.springlibrary.librarybespring.requestmodels.AdminQuestionRequest;
 import mjz.springlibrary.librarybespring.service.MessagesService;
 import mjz.springlibrary.librarybespring.utils.ExtractJWT;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,22 @@ public class MessagesController {
 
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         messagesService.postMessage(messageRequest, userEmail);
+
+    }
+
+    @PutMapping("/secure/admin/message")
+    public void putMessage(@RequestHeader(value = "Authorization") String token,
+                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        // check if the user is "admin" check the "5. Create Admin User" for creating an admin user and adding the userType to "claim" / payload in okta settings
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+
+        if(admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+
+        messagesService.putMessage(adminQuestionRequest, userEmail);
 
     }
 }
