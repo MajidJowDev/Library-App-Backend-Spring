1afecc02dd378db9207a55c9bf1969a6d2d684ca
MajidJowDev <majid.jow@gmail.com>
2023-03-20 14:55:21 +0330

    27- 6. Spring Boot - Open Ticket Endpoint

--- src/main/java/mjz/springlibrary/librarybespring/dao/MessageRepository.java
+++ src/main/java/mjz/springlibrary/librarybespring/dao/MessageRepository.java
@@ -9,4 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
+
+    // admin will use this endpoint to see the messages (find it by all users)
+    Page<Message> findByClosed(@RequestParam("closed") boolean closed, Pageable pageable);
 }
