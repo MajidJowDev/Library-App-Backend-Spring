12784fea536d9b597e6420f6c022818b4c5411ee
MajidJowDev <majid.jow@gmail.com>
2023-03-22 01:53:26 +0430

    29- 9. Spring Boot - Enhance Repositories for Delete Added delete method to CheckoutRepository and ReviewRepository

--- src/main/java/mjz/springlibrary/librarybespring/dao/CheckoutRepository.java
+++ src/main/java/mjz/springlibrary/librarybespring/dao/CheckoutRepository.java
@@ -2,6 +2,9 @@ package mjz.springlibrary.librarybespring.dao;
 
 import mjz.springlibrary.librarybespring.entity.Checkout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +13,8 @@ public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
     Checkout findByUserEmailAndBookId(String userEmail, Long bookId);
 
     List<Checkout> findBooksByUserEmail(String userEmail); // get a list of books that the user have checked out
+
+    @Modifying // we are going to delete (modify) the record
+    @Query("delete from Checkout c where c.bookId in :book_id")
+    void deleteAllByBookId(@Param("book_id") Long bookId);
 }
